3f8a6fc1262bd44ac0df8a01e67371b1e16bdcf1
kristofav <kovacsk3@kkszki.hu>
2026-01-28 16:42:00 +0100

    alma class

--- almaOOP/alma.cs
+++ almaOOP/alma.cs
@@ -7,7 +7,24 @@ using System.Threading.Tasks;
 namespace almaOOP
 {
     internal class alma
-    
+    {
+        public string nev;
+        public string szin;
+        public double suly;
+        public string fajta;
+        
+        public alma(string nev,string szin, double suly, string fajta)
+        {
+            this.nev = nev;
+            this.szin = szin;
+            this.suly = suly;
+            this.fajta = fajta;
+        }
+
+        public override string ToString()
+        {
+            return $"Gyümölocs neve?: {nev}, Szyne: {szin}, súlya: {suly}g, Fajta: {fajta}";
+        }
+
     }
 }
-
