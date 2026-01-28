52b0e5573f806233e2713679d9f2191ed02a3cd4
kristofav <kovacsk3@kkszki.hu>
2026-01-28 16:28:24 +0100

    gyümölcs class

---
+++ almaOOP/gyümölcs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace almaOOP
+{
+    internal class gyümölcs
+    {
+        public class Gyümölcs
+        {
+            public string nev;
+            public string szin;
+            public string suly;
+
+            public Gyümölcs()
+            {
+            }
+
+            public Gyümölcs(string nev, string szin, string suly)
+            {
+                this.nev = nev;
+                this.szin = szin;
+                this.suly = suly;
+            }
+
+            public override string ToString()
+            {
+                return $"Gyümölocs neve?: {nev}, Szyne: {szin}, súlya: {suly}g";
+            }
+        }
+    }
+}
